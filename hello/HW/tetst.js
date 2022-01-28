6869f889a0cb2650ff8f2a3c70441f00a3324e04
ltybcltheyjd <d.derunov@student.csn.khai.edu>
2022-01-28 17:44:11 +0200

    Home Work 28/01/22

---
+++ hello/HW/tetst.js
@@ -0,0 +1,24 @@
+// let a = process.argv[2];
+// let b = process.argv[3];
+// let c = process.argv[4];
+
+// let quadEquationSolver = function (a, b, c) {
+//     let rootPart = Math.sqrt( (b * b) - (4 * a * c) );
+//     let denom = 2 * a;
+    
+//     let firstRoot = (-b + rootPart)/denom;
+//     let secondRoot = (-b - rootPart)/denom;
+    
+//     console.log("The first root is " + firstRoot);
+//     console.log("The second root is " + secondRoot);
+//   };
+
+//   quadEquationSolver(a, b, c);
+
+
+// for (let i = 0; i <= 10; i++){
+//     console.log(i);
+// }
+
+let b = '';
+console.log(typeof(b));
